6c82edb2dd6a7dd605576db539c2e581f49b3a52
eoin.mcallister <eoin.mcallister@airtel-atn.com>
2019-07-04 16:51:56 +0100

    commit

--- Iris Flower Classification/iris_flowers.py
+++ Iris Flower Classification/iris_flowers.py
@@ -16,44 +16,45 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
 # Pre process the data
-data = pd.read_csv("IRIS.csv")
-X = data.iloc[:, :-1].values
-y = data.iloc[:, -1].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+DATA = pd.read_csv("IRIS.csv")
+X = DATA.iloc[:, :-1].values
+Y = DATA.iloc[:, -1].values
+X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, test_size=0.2)
 
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+SC = StandardScaler()
+X_TRAIN = SC.fit_transform(X_TRAIN)
+X_TEST = SC.transform(X_TEST)
 
 # Classifiers
 classifiers = []
-xgb_class = XGBClassifier()
-svm_class = svm.SVC()
-tree_class = tree.DecisionTreeClassifier()
-forest_class = RandomForestClassifier(n_estimators=100)
-naive_class = GaussianNB()
-kmeans_class = KNeighborsClassifier()
-regression_class = LogisticRegression(solver="lbfgs", multi_class="auto")
+XGB_CLASS = XGBClassifier()
+SVM_CLASS = svm.SVC()
+TREE_CLASS = tree.DecisionTreeClassifier()
+FOREST_CLASS = RandomForestClassifier(n_estimators=100)
+NAIVE_CLASS = GaussianNB()
+K_MEANS_CLASS = KNeighborsClassifier()
+REGRESSION_CLASS = LogisticRegression(solver="lbfgs", multi_class="auto")
 
-classifiers.append(xgb_class)
-classifiers.append(svm_class)
-classifiers.append(tree_class)
-classifiers.append(forest_class)
-classifiers.append(naive_class)
-classifiers.append(kmeans_class)
-classifiers.append(regression_class)
+classifiers.append(XGB_CLASS)
+classifiers.append(SVM_CLASS)
+classifiers.append(TREE_CLASS)
+classifiers.append(FOREST_CLASS)
+classifiers.append(NAIVE_CLASS)
+classifiers.append(K_MEANS_CLASS)
+classifiers.append(REGRESSION_CLASS)
 
 accuracy_list = []
 cm_list = []
 
 for clf in classifiers:
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    cm = confusion_matrix(y_test, y_pred)
+    clf.fit(X_TRAIN, Y_TRAIN)
+    y_pred = clf.predict(X_TEST)
+    acc = accuracy_score(Y_TEST, y_pred)
+    cm = confusion_matrix(Y_TEST, y_pred)
     accuracy_list.append([acc])
     cm_list.append([cm])
 
 most_accurate_class = classifiers[accuracy_list.index(max(accuracy_list))]
 accuarcy_percent = max(accuracy_list)
-print("The most accurate classifier is the", most_accurate_class, " with and accuracy of", accuarcy_percent)
+print("The most accurate classifier is the", most_accurate_class, " with and accuracy of",
+      accuarcy_percent)
